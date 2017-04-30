32066d7cea3c3a3d08f0ba64abc3f9331b93422d
Kit Lawes <kitlawes@hotmail.com>
2017-04-30 18:13:05 +0100

    Add SameGameBoard as a data structure for the game board, modify SameGameView to display the game board, and modify SameGameDoc to interface between SameGameBoard and SameGameView

--- SameGame/SameGameView.cpp
+++ SameGame/SameGameView.cpp
@@ -1,13 +1,5 @@
-
-// SameGameView.cpp : implementation of the CSameGameView class
-//
-
 #include "stdafx.h"
-// SHARED_HANDLERS can be defined in an ATL project implementing preview, thumbnail
-// and search filter handlers and allows sharing of document code with that project.
-#ifndef SHARED_HANDLERS
 #include "SameGame.h"
-#endif
 
 #include "SameGameDoc.h"
 #include "SameGameView.h"
@@ -16,20 +8,14 @@
 #define new DEBUG_NEW
 #endif
 
-
 // CSameGameView
-
 IMPLEMENT_DYNCREATE(CSameGameView, CView)
-
 BEGIN_MESSAGE_MAP(CSameGameView, CView)
 END_MESSAGE_MAP()
 
 // CSameGameView construction/destruction
-
 CSameGameView::CSameGameView()
 {
-	// TODO: add construction code here
-
 }
 
 CSameGameView::~CSameGameView()
@@ -38,27 +24,57 @@ CSameGameView::~CSameGameView()
 
 BOOL CSameGameView::PreCreateWindow(CREATESTRUCT& cs)
 {
-	// TODO: Modify the Window class or styles here by modifying
-	//  the CREATESTRUCT cs
-
 	return CView::PreCreateWindow(cs);
 }
 
 // CSameGameView drawing
 
-void CSameGameView::OnDraw(CDC* /*pDC*/)
+void CSameGameView::OnDraw(CDC* pDC) // MFC will comment out the argument name by default; uncomment it
 {
+	//  First get a pointer to the document
 	CSameGameDoc* pDoc = GetDocument();
 	ASSERT_VALID(pDoc);
 	if (!pDoc)
 		return;
-
-	// TODO: add draw code for native data here
+	//  Save the current state of the device context
+	int nDCSave = pDC->SaveDC();
+	//  Get the client rectangle
+	CRect rcClient;
+	GetClientRect(&rcClient);
+	//  Get the background color of the board
+	COLORREF clr = pDoc->GetBoardSpace(-1, -1);
+	//	Draw the background first
+	pDC->FillSolidRect(&rcClient, clr);
+	//  Create the brush for drawing
+	CBrush br;
+	br.CreateStockObject(HOLLOW_BRUSH);
+	CBrush* pbrOld = pDC->SelectObject(&br);
+	//	Draw the squares
+	for (int row = 0; row < pDoc->GetRows(); row++)
+	{
+		for (int col = 0; col < pDoc->GetColumns(); col++)
+		{
+			//  Get the color for this board space
+			clr = pDoc->GetBoardSpace(row, col);
+			//  Calculate the size and position of this space
+			CRect rcBlock;
+			rcBlock.top = row * pDoc->GetHeight();
+			rcBlock.left = col * pDoc->GetWidth();
+			rcBlock.right = rcBlock.left + pDoc->GetWidth();
+			rcBlock.bottom = rcBlock.top + pDoc->GetHeight();
+			//  Fill in the block with the correct color
+			pDC->FillSolidRect(&rcBlock, clr);
+			//  Draw the block outline
+			pDC->Rectangle(&rcBlock);
+		}
+	}
+	//  Restore the device context settings
+	pDC->RestoreDC(nDCSave);
+	br.DeleteObject();
 }
 
 
 // CSameGameView diagnostics
-
 #ifdef _DEBUG
 void CSameGameView::AssertValid() const
 {
@@ -70,12 +86,43 @@ void CSameGameView::Dump(CDumpContext& dc) const
 	CView::Dump(dc);
 }
 
-CSameGameDoc* CSameGameView::GetDocument() const // non-debug version is inline
+//  non-debug version is inline
+CSameGameDoc* CSameGameView::GetDocument() const
 {
 	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CSameGameDoc)));
 	return (CSameGameDoc*)m_pDocument;
 }
 #endif //_DEBUG
 
+void CSameGameView::OnInitialUpdate()
+{
+	CView::OnInitialUpdate();
 
-// CSameGameView message handlers
+	//  Resize the window
+	ResizeWindow();
+
+}
+
+
+void CSameGameView::ResizeWindow()
+{
+	//  First get a pointer to the document
+	CSameGameDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+	//  Get the size of the client area and the window
+	CRect rcClient, rcWindow;
+	GetClientRect(&rcClient);
+	GetParentFrame()->GetWindowRect(&rcWindow);
+	//  Calculate the difference
+	int nWidthDiff = rcWindow.Width() - rcClient.Width();
+	int nHeightDiff = rcWindow.Height() - rcClient.Height();
+	//  Change the window size based on the size of the game board
+	rcWindow.right = rcWindow.left +
+		pDoc->GetWidth() * pDoc->GetColumns() + nWidthDiff;
+	rcWindow.bottom = rcWindow.top +
+		pDoc->GetHeight() * pDoc->GetRows() + nHeightDiff;
+	//  The MoveWindow function resizes the frame window
+	GetParentFrame()->MoveWindow(&rcWindow);
+}
--- SameGame/SameGameView.h
+++ SameGame/SameGameView.h
@@ -1,9 +1,4 @@
-
-// SameGameView.h : interface of the CSameGameView class
-//
-
 #pragma once
-
 
 class CSameGameView : public CView
 {
@@ -11,36 +6,36 @@ protected: // create from serialization only
 	CSameGameView();
 	DECLARE_DYNCREATE(CSameGameView)
 
-// Attributes
+	// Attributes
 public:
 	CSameGameDoc* GetDocument() const;
-
-// Operations
-public:
-
-// Overrides
+	// Overrides
 public:
 	virtual void OnDraw(CDC* pDC);  // overridden to draw this view
 	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
 protected:
 
-// Implementation
+	// Implementation
 public:
+
+	void ResizeWindow();
+
 	virtual ~CSameGameView();
 #ifdef _DEBUG
 	virtual void AssertValid() const;
 	virtual void Dump(CDumpContext& dc) const;
 #endif
 
-protected:
-
-// Generated message map functions
+	// Generated message map functions
 protected:
 	DECLARE_MESSAGE_MAP()
+public:
+	virtual void OnInitialUpdate();
 };
 
 #ifndef _DEBUG  // debug version in SameGameView.cpp
 inline CSameGameDoc* CSameGameView::GetDocument() const
-   { return reinterpret_cast<CSameGameDoc*>(m_pDocument); }
+{
+	return reinterpret_cast<CSameGameDoc*>(m_pDocument);
+}
 #endif
-
